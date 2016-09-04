52650cf3119fb4ec13a60cd662ed8b44818e64dc
ambrulio <giulio.ambrogi@gmail.com>
2016-09-05 00:12:28 +0100

    Refactoring + bug fixing on portfolio boxes

---
+++ gulpfile.js
@@ -0,0 +1,88 @@
+'use strict';
+
+var fs                 = require("fs"),
+    gulp               = require('gulp'),
+    rename             = require("gulp-rename"),
+    browserify         = require("browserify"),
+    source             = require('vinyl-source-stream'),
+    sass               = require('gulp-sass'),
+    prefix             = require('gulp-autoprefixer'),
+    buffer             = require('vinyl-buffer'),
+    minifyCSS          = require('gulp-cssnano'),
+    size               = require('gulp-filesize'),
+    uglify             = require('gulp-uglify'),
+    gutil              = require('gulp-util'),
+    svgstore           = require('gulp-svgstore'),
+    svgmin             = require('gulp-svgmin'),
+    del                = require('del'),
+    mkdirp             = require('mkdirp'),
+    runSequence        = require('run-sequence'),
+    path               = require('path'),
+    cheerio            = require('gulp-cheerio'),
+    $                  = require('jquery'),
+    inject             = require('gulp-inject'),
+    sources            = {
+      scripts: {
+        path: './public/assets/src/javascripts',
+        entries: './public/assets/src/javascripts/application.js',
+        dist_path: './public/assets/dist/javascripts'
+      },
+      css: {
+        src_path: './public/assets/src/scss/**/*.scss',
+        dist_path: './public/assets/dist/css'
+      },
+      asset_path: './public/assets/dist/**'
+    };
+
+var onerror = function(err) {
+  if (err) gutil.log(gutil.colors.magenta('!! Error'), ':', gutil.colors.cyan(err.plugin), '-', gutil.colors.red(err.message));
+    console.log(err)
+};
+
+gulp.task('scripts', function() {
+  return browserify({
+    entries: [sources.scripts.entries],
+    paths: sources.scripts.path
+  })
+    .bundle()
+    .pipe(source('application.min.js'))
+    .pipe(buffer())
+    .pipe(uglify())
+    .pipe(gulp.dest(sources.scripts.dist_path));
+});
+
+gulp.task('scripts-dev', function() {
+  var b = browserify({
+    entries: [sources.scripts.entries],
+    paths: ['./node_modules', sources.scripts.path],
+    debug: true,
+    cache: {},
+    packageCache: {},
+    fullPaths: true
+  });
+
+  return b.bundle()
+    .pipe(source('application.js'))
+    .pipe(gulp.dest(sources.scripts.dist_path));
+});
+
+gulp.task('styles', function() {
+  gulp.src([sources.css.src_path])
+    .pipe(sass())
+    .on('error', onerror)
+    .pipe(prefix("last 3 version", "> 1%", "ie 9"))
+    .pipe(minifyCSS())
+    .pipe(gulp.dest(sources.css.dist_path))
+    .pipe(size());
+});
+
+
+
+gulp.task('watch', function() {
+  gulp.watch('./public/assets/src/javascripts/**/*', ['scripts-dev']);
+  gulp.watch('./public/assets/src/scss/**/*.scss', ['styles']);
+});
+
+gulp.task('default', ['watch']);
+
+
