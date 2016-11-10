1263219e293deb01fc38c82b95ec8a9d111bdffa
ambrulio <giulio.ambrogi@gmail.com>
2016-11-10 14:06:30 +0000

    Uploaded new pdf

--- public/assets/src/javascripts/components/hero.js
+++ public/assets/src/javascripts/components/hero.js
@@ -1,17 +1,17 @@
 
 var $ = require('jquery');
 var typed = require('../vendor/typed');
-var typed_speed = 30;
+var typed_speed = 20;
 module.exports = (function(){
   function init() {
     $(document).ready(function(){
 
-      $(".header-content h3").typed({
+      $(".header-content p.hi").typed({
         strings: ["Hi there,"],
         typeSpeed: typed_speed,
         callback: function(){
-          $(".header-content h3").next(".typed-cursor").hide();
-             $(".header-content p").typed({
+          $(".header-content p.hi").next(".typed-cursor").hide();
+             $(".header-content p.iam").typed({
               strings: ["I am Giulio, web developer based in London."],
               typeSpeed: typed_speed,
               callback: function(){
